3af23fb6177294ae0ade5ef8986c56b5d2a89d7b
Alexander1455 <alexander21111121@gmail.com>
2023-11-06 02:20:49 -0500

    custom template request

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -23,26 +23,20 @@ def close_db(error):
 
 
 @app.route('/100-hbnb', strict_slashes=False)
-def hbnb():
-    """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
+def hbnb_filters(the_id=None):
+    """ request to custom template with states, cities & amentities """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amenities = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
     return render_template('100-hbnb.html',
-                           states=st_ct,
+                           states=states,
                            amenities=amenities,
                            places=places,
-                           cache_id=str(uuid.uuid4()))
+                           users=users,
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
